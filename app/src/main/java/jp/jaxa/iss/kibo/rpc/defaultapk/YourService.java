7eb035d6fbe21cf1f145fc7173cf95ad9d506094
ColasssNotMe <81697922+ColasssNotMe@users.noreply.github.com>
2024-05-23 18:46:50 +0800

    get to 1st point and log the image

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -1,6 +1,8 @@
 package jp.jaxa.iss.kibo.rpc.defaultapk;
 
 import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
+import gov.nasa.arc.astrobee.types.Point;
+import org.opencv.core.Mat;
 
 /**
  * Class meant to handle commands from the Ground Data System and execute them in Astrobee
@@ -10,20 +12,28 @@ public class YourService extends KiboRpcService {
 
     //plan 1
     @Override
-    protected void runPlan1(){
-        double x;
-        double y;
-        double z;
-        
+    protected void runPlan1() {
+        api.startMission();
+        Point point = new Point(0, 0, 0);
+        api.moveTo(point,null, false);
+        Mat image = api.getMatNavCam();
+            if(image==null){
+                api.flashlightControlFront(80);
+            }else{
+                api.flashlightControlFront(10);
+                api.getMatNavCam();
+            }
+
+
     }
 
     @Override
-    protected void runPlan2(){
+    protected void runPlan2() {
         // write your plan 2 here
     }
 
     @Override
-    protected void runPlan3(){
+    protected void runPlan3() {
         // write your plan 3 here
     }
 
