d9b6ace2f9ac9305c9f6c5561b3c1e086ce93b9e
ColasssNotMe <81697922+ColasssNotMe@users.noreply.github.com>
2024-05-23 17:51:45 +0800

    push 1st file

---
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -0,0 +1,31 @@
+package jp.jaxa.iss.kibo.rpc.defaultapk;
+
+import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
+
+/**
+ * Class meant to handle commands from the Ground Data System and execute them in Astrobee
+ */
+
+public class YourService extends KiboRpcService {
+
+    //plan 1
+    @Override
+    protected void runPlan1(){
+        double x;
+        double y;
+        double z;
+        
+    }
+
+    @Override
+    protected void runPlan2(){
+        // write your plan 2 here
+    }
+
+    @Override
+    protected void runPlan3(){
+        // write your plan 3 here
+    }
+
+}
+
